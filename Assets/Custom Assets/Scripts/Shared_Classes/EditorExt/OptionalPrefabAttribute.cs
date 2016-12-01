b8fa267c2cb7f4fe5adcbff963c75b6f7600862e
FFouetil <fabien.fouetillou@gmail.com>
2016-12-01 14:45:01 +0100

    created file structure in Custom Assets, added useful plugins and classes

---
+++ Assets/Custom Assets/Scripts/Shared_Classes/EditorExt/OptionalPrefabAttribute.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using Object = UnityEngine.Object;
+#if UNITY_EDITOR && false
+using UnityEditor;
+//#endif
+
+public class OptionalPrefabAttribute : PropertyAttribute {
+
+	public string path;
+
+	public OptionalPrefabAttribute(string prefabPath)
+	{
+		path = prefabPath;
+		
+	}
+}
+
+//#if UNITY_EDITOR
+
+[CustomPropertyDrawer(typeof(OptionalPrefabAttribute))]
+//[CustomPropertyDrawer(typeof(UnityEngine.Object))]
+public class OptionalPrefabDrawer : PropertyDrawer{
+
+	const int checkBoxWidth = 20;
+	public bool usePrefab;
+
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		/*
+		//base.OnGUI(position, property, label);
+		Rect obj = position;
+		obj.width -= checkBoxWidth;
+	//	obj.x += checkBoxWidth;
+
+		Rect chk = position;
+		chk.width -= obj.width;
+		
+		//position.x = label.image.width;
+
+		if (EditorGUI.Toggle(chk, label,!usePrefab))
+		{
+			usePrefab = !usePrefab;
+		}
+		
+		EditorGUI.ObjectField(obj, property, GUIContent.none);
+		*/
+		Rect r=EditorGUILayout.BeginHorizontal();
+		if (EditorGUILayout.BeginToggleGroup(label, usePrefab=!usePrefab)) { 
+}
+		//EditorGUI.ObjectField(obj, property, GUIContent.none);
+		//System.Type type = property.propertyType.GetType();		
+
+		var o=EditorGUILayout.ObjectField(property.objectReferenceValue, this.fieldInfo.FieldType, true);
+		EditorGUILayout.EndToggleGroup();
+		EditorGUILayout.EndHorizontal();
+
+	}
+
+
+}
+
+#endif
+
