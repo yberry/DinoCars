f99f8068de4a3559bde623897ed387615b64762a
yberry <y_berry@hotmail.fr>
2016-12-08 17:50:51 +0100

    Anim dino + collider

---
+++ Assets/Dino/DinoCollider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SkinnedMeshRenderer), typeof(MeshCollider))]
+public class DinoCollider : MonoBehaviour {
+
+    public uint freqRefreshCol = 2;
+
+    SkinnedMeshRenderer skin;
+    Mesh mesh;
+    MeshCollider col;
+
+    uint fr = 0;
+
+    void Start()
+    {
+        skin = GetComponent<SkinnedMeshRenderer>();
+        mesh = new Mesh();
+        col = GetComponent<MeshCollider>();
+    }
+
+    void LateUpdate()
+    {
+        if (fr % freqRefreshCol == 0)
+        {
+            skin.BakeMesh(mesh);
+            col.sharedMesh = null;
+            col.sharedMesh = mesh;
+        }
+        fr++;
+    }
+    
+}
