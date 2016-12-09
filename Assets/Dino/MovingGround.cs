6fd3dc49a837546ff82acdf3b312dd7ac3931c94
yberry <y_berry@hotmail.fr>
2016-12-09 15:40:59 +0100

    cylindre

---
+++ Assets/Dino/MovingGround.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingGround : MonoBehaviour {
+
+    public Vector3 maxRotation;
+    public float speed = 1f;
+
+    Quaternion initialRotation;
+    float time = 0f;
+
+	// Use this for initialization
+	void Start () {
+        initialRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime * speed;
+        Vector3 tmpVect = Mathf.Sin(time) * maxRotation;
+        transform.rotation = initialRotation * Quaternion.Euler(tmpVect);
+	}
+}
