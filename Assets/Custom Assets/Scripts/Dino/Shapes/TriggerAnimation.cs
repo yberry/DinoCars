80d2c0fa41380f7b4c005d41514a345a1cda6bfd
yberry <y_berry@hotmail.fr>
2017-01-10 17:46:23 +0100

    Voiture scriptée

---
+++ Assets/Custom Assets/Scripts/Dino/Shapes/TriggerAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimation : MonoBehaviour {
+
+    public MegaShape megaShape;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            megaShape.animate = true;
+            Destroy(gameObject);
+        }
+    }
+}
