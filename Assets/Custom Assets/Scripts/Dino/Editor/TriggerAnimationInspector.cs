596664b93eeb360a9be0fded585f3485e4fefe2f
yberry <y_berry@hotmail.fr>
2017-01-11 15:41:06 +0100

    Add TriggerAnimation

---
+++ Assets/Custom Assets/Scripts/Dino/Editor/TriggerAnimationInspector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TriggerAnimation))]
+public class TriggerAnimationInspector : Editor {
+
+    TriggerAnimation triggerAnimation;
+
+    public override void OnInspectorGUI()
+    {
+        triggerAnimation = target as TriggerAnimation;
+
+        EditorGUI.BeginChangeCheck();
+        MegaShapeLoft loft = (MegaShapeLoft)EditorGUILayout.ObjectField("Loft", triggerAnimation.loft, typeof(MegaShapeLoft), true);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(triggerAnimation, "Loft");
+            EditorUtility.SetDirty(triggerAnimation);
+            triggerAnimation.loft = loft;
+        }
+
+        EditorGUI.BeginChangeCheck();
+        int layer = MegaShapeUtils.FindLayer(loft, triggerAnimation.layer);
+        layer = EditorGUILayout.Popup("Layer", layer + 1, MegaShapeUtils.GetLayers(loft)) - 1;
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(triggerAnimation, "Loft layer");
+            EditorUtility.SetDirty(triggerAnimation);
+            triggerAnimation.layer = layer;
+        }
+    }
+}
--- Assets/Custom Assets/Scripts/Dino/Shapes/TriggerAnimation.cs
+++ Assets/Custom Assets/Scripts/Dino/Shapes/TriggerAnimation.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class TriggerAnimation : MonoBehaviour {
 
-    public MegaShape megaShape;
+    public MegaShapeLoft loft;
+    public int layer;
 
     void OnTriggerEnter(Collider col)
     {
         if (col.tag == "Player")
         {
-            megaShape.animate = true;
+            loft.DoCollider = true;
+            loft.Layers[0].layerPath.animate = true;
             Destroy(gameObject);
         }
     }
