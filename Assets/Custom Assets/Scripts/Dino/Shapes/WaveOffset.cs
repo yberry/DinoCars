3d90067097eca36fcb0ee4c9fc07ab452b071d9d
yberry <y_berry@hotmail.fr>
2017-01-13 13:04:40 +0100

    Update curve

--- Assets/Custom Assets/Scripts/Dino/Shapes/WaveOffset.cs
+++ Assets/Custom Assets/Scripts/Dino/Shapes/WaveOffset.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class WaveOffset : MonoBehaviour {
 
     public MegaShapeLoft loft;
@@ -13,24 +14,85 @@ public class WaveOffset : MonoBehaviour {
     public float amplitude;
     public float gap;
 
-    AnimationCurve curve;
-
-    void Awake()
+    MegaLoftLayerSimple megaLayer;
+    AnimationCurve Curve
     {
-        MegaLoftLayerSimple megaLayer = loft.Layers[layer] as MegaLoftLayerSimple;
-        switch (axis)
+        get
         {
-            case MegaAxis.X:
-                curve = megaLayer.offsetCrvX;
-                break;
+            switch (axis)
+            {
+                case MegaAxis.X:
+                    return megaLayer.offsetCrvX;
 
-            case MegaAxis.Y:
-                curve = megaLayer.offsetCrvY;
-                break;
+                case MegaAxis.Y:
+                    return megaLayer.offsetCrvY;
 
-            case MegaAxis.Z:
-                curve = megaLayer.offsetCrvZ;
-                break;
+                case MegaAxis.Z:
+                    return megaLayer.offsetCrvZ;
+
+                default:
+                    return null;
+            }
+        }
+
+        set
+        {
+            switch (axis)
+            {
+                case MegaAxis.X:
+                    megaLayer.offsetCrvX = value;
+                    megaLayer.useOffsetX = true;
+                    break;
+
+                case MegaAxis.Y:
+                    megaLayer.offsetCrvY = value;
+                    megaLayer.useOffsetY = true;
+                    break;
+
+                case MegaAxis.Z:
+                    megaLayer.offsetCrvZ = value;
+                    megaLayer.useOffsetZ = true;
+                    break;
+            }
+        }
+    }
+    float time = 0f;
+    bool active = false;
+
+    void Start()
+    {
+        megaLayer = loft.Layers[layer] as MegaLoftLayerSimple;
+
+        AnimationCurve curve = new AnimationCurve();
+
+        if (startToEnd)
+        {
+            curve.AddKey(0f, 0f);
+            curve.AddKey(gap / 2f, 0f);
+            curve.AddKey(gap, 0f);
+        }
+        else
+        {
+            curve.AddKey(1f - gap, 0f);
+            curve.AddKey(1f - gap / 2f, 0f);
+            curve.AddKey(1f, 0f);
+        }
+
+        Curve = curve;
+
+        Debug.Log(Curve.length);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        active = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (active)
+        {
+
         }
     }
 }
