bbd18dc71948b0f232abb661de0026495b59af1d
yberry <y_berry@hotmail.fr>
2017-01-13 17:14:48 +0100

    ça build !

---
+++ Assets/Custom Assets/Scripts/Shared_Classes/AutoInstantiateAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoInstantiateAttribute : PropertyAttribute
+{
+
+    public AutoInstantiateAttribute()
+    {
+        System.Console.Write("DFSGSDFDSF");
+    }
+}
