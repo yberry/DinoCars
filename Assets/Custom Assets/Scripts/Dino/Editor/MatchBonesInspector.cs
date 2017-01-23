b910024e3914ec87c452b780c44bcc0b2dbce4f8
yberry <y_berry@hotmail.fr>
2017-01-23 18:33:37 +0100

    Start matching bones

---
+++ Assets/Custom Assets/Scripts/Dino/Editor/MatchBonesInspector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MatchBones))]
+public class MatchBonesInspector : Editor {
+
+    MatchBones matchBones;
+
+    public override void OnInspectorGUI()
+    {
+        matchBones = target as MatchBones;
+
+        EditorGUI.BeginChangeCheck();
+        MegaShape shape = (MegaShape)EditorGUILayout.ObjectField("Shape", matchBones.shape, typeof(MegaShape), true);
+        if (EditorGUI.EndChangeCheck())
+        {
+            matchBones.Record("Shape");
+            matchBones.shape = shape;
+        }
+
+        if (shape.splines.Count > 1)
+        {
+            matchBones.UpdateInt(ref matchBones.spline, "Spline", 0, shape.splines.Count - 1);
+        }
+        else
+        {
+            matchBones.spline = 0;
+        }
+
+        SerializedProperty boneKnots = serializedObject.FindProperty("boneKnots");
+        EditorGUI.BeginChangeCheck();
+        EditorGUILayout.PropertyField(boneKnots, true);
+        if (EditorGUI.EndChangeCheck())
+        {
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Custom Assets/Scripts/Dino/Shapes/MatchBones.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchBones : MonoBehaviour {
+
+    [System.Serializable]
+    public struct BoneKnot
+    {
+        public Transform bone;
+        public int knot;
+    }
+
+    public MegaShape shape;
+    public int spline;
+    public BoneKnot[] boneKnots;
+
+    Transform shapeTr;
+    MegaSpline megaSpline;
+
+    void Awake()
+    {
+        megaSpline = shape.splines[spline];
+        shapeTr = shape.transform;
+    }
+    
+    void Update()
+    {
+        foreach (BoneKnot bk in boneKnots)
+        {
+            UpdateKnot(bk);
+        }
+    }
+
+    void UpdateKnot(BoneKnot bk)
+    {
+        megaSpline.knots[bk.knot].p = bk.bone.position;
+        if (bk.knot == 0)
+        {
+            Debug.Log(bk.bone.position);
+        }
+    }
+}
