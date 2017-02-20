cbee9ca8e2881b1e59731bb56565c7de141aa4c7
yberry <y_berry@hotmail.fr>
2017-02-20 16:56:15 +0100

    Add new menu

---
+++ Assets/Custom Assets/Scripts/UI/Quit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Quit : MonoBehaviour {
+
+    public Button quit;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            quit.onClick.Invoke();
+        }
+    }
+}
