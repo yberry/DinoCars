9f956ced276edc12fd7bb5368e1816b289584091
FFouetil <fabien.fouetillou@gmail.com>
2017-02-20 17:03:13 +0100

    stuff for victoria

---
+++ Assets/Custom Assets/Scripts/Car/CarStateManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CND.Car
+{
+	public class CarStateManager : MonoBehaviour
+	{
+
+		public float LastTimeSpawned { get; protected set; }
+		public float TimeSinceLastSpawn { get { return Time.time - LastTimeSpawned; } }
+
+		ArcadeCarController car;
+		// Use this for initialization
+		void Start()
+		{
+			if (!car)
+				car = GetComponent<ArcadeCarController>();
+
+			enabled = car;
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		public void Spawn()
+		{
+			LastTimeSpawned = Time.time;
+		}
+	}
+
+}
