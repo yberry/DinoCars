32174380a3f4b5d5e2fea0183d480f720c9e25f1
yberry <y_berry@hotmail.fr>
2017-02-21 18:37:30 +0100

    Anims menu !

--- Assets/Custom Assets/Scripts/UI/MainMenu.cs
+++ Assets/Custom Assets/Scripts/UI/MainMenu.cs
@@ -10,6 +10,10 @@ public class MainMenu : MonoBehaviour {
     public Camera cameraVHS;
     public Text canal;
 
+    public RectTransform telDefault;
+    public RectTransform telVire;
+    public RectTransform telStandby;
+
     RectTransform currentMenu;
 
     void Start()
@@ -20,13 +24,45 @@ public class MainMenu : MonoBehaviour {
 
     public void ChangeTo(RectTransform newMenu)
     {
+        StartCoroutine(Anim(newMenu));
+    }
+
+    IEnumerator Anim(RectTransform newMenu)
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+
         if (currentMenu != null)
         {
+            ManageButtons manageCurrent = currentMenu.GetComponent<ManageButtons>();
+            if (manageCurrent != null)
+            {
+                manageCurrent.telecommande.position = telDefault.position;
+                manageCurrent.telecommande.rotation = telDefault.rotation;
+
+                manageCurrent.targetTel = Instantiate(telVire);
+                manageCurrent.targetTel.position = telVire.position;
+                manageCurrent.targetTel.rotation = telVire.rotation;
+
+                yield return new WaitUntil(() => manageCurrent.telCheck);
+            }
             currentMenu.gameObject.SetActive(false);
         }
 
         if (newMenu != null)
         {
+            ManageButtons manageNew = newMenu.GetComponent<ManageButtons>();
+            if (manageNew != null)
+            {
+                manageNew.telecommande.position = telStandby.position;
+                manageNew.telecommande.rotation = telStandby.rotation;
+
+                manageNew.targetTel = Instantiate(telDefault);
+                manageNew.targetTel.position = telDefault.position;
+                manageNew.targetTel.rotation = telDefault.rotation;
+
+                newMenu.gameObject.SetActive(true);
+                yield return new WaitUntil(() => manageNew.telCheck);
+            }
             newMenu.gameObject.SetActive(true);
             currentMenu = newMenu;
             SetSelection();
--- Assets/Custom Assets/Scripts/UI/ManageButtons.cs
+++ Assets/Custom Assets/Scripts/UI/ManageButtons.cs
@@ -7,16 +7,32 @@ public class ManageButtons : MonoBehaviour {
 
     public Button quit;
 
+    public RectTransform telecommande;
+
+    public RectTransform targetTel { get; set; }
+
+    public bool telCheck
+    {
+        get
+        {
+            return telecommande.position == targetTel.position && telecommande.rotation == targetTel.rotation;
+        }
+    }
+
     void Start()
     {
         foreach (Button button in buttons)
         {
             button.gameObject.AddComponent<EventButton>();
         }
+        quit.gameObject.AddComponent<EventButton>();
     }
 
     void Update()
     {
+        telecommande.position = Vector3.MoveTowards(telecommande.position, targetTel.position, 1000f * Time.deltaTime);
+        telecommande.rotation = Quaternion.RotateTowards(telecommande.rotation, targetTel.rotation, 1000f * Time.deltaTime);
+
         if (Input.GetButtonDown("Cancel"))
         {
             quit.onClick.Invoke();
