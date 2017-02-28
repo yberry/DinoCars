54a306f905c3127691a64da770cbc12e9cfa975c
FFouetil <fabien.fouetillou@gmail.com>
2017-02-28 11:12:22 +0100

    cool splosion + fix query hit triggers

--- Assets/Custom Assets/Scripts/Car/CarStateManager.cs
+++ Assets/Custom Assets/Scripts/Car/CarStateManager.cs
@@ -12,9 +12,14 @@ namespace CND.Car
 		public float TimeSinceLastSpawn { get { return Time.time - LastTimeSpawned; } }
 
 
-
+		[Header("Damage management")]
+		[Range(0,1000)]
+		public float minExplosionVelocity=50;
+		public bool ShouldPlayExplosion { get; set; }
+		[Range(0,10)]
 		public float fadeDuration = 1.5f;
 		public float FadeProgress { get { return fadeTimer / fadeDuration; } }
+		public FXManager explosionFXManager;
 
 
 		float fadeTimer;
@@ -32,7 +37,8 @@ namespace CND.Car
 		// Update is called once per frame
 		void Update()
 		{
-			fadeTimer += Time.deltaTime;
+			if(ShouldPlayExplosion)
+				fadeTimer += Time.deltaTime;
 		}
 
 		public void Spawn(Vector3 position, Quaternion rotation)
@@ -56,17 +62,75 @@ namespace CND.Car
 		internal void Explode()
 		{
 			ResetFX();
+			ShouldPlayExplosion = true;
 			var exp = GetComponentInChildren<PseudoVolumetricExplosion>();
 			if (exp)
 			{
 				exp.enabled = true;
 				exp.Play();
 			}
+
+			if (explosionFXManager)
+			{
+				explosionFXManager.PlayOnce();
+			}
 		}
 
 		void ResetFX()
 		{
 			fadeTimer = 0;
+		}
+
+
+
+		void Collision_CheckShouldExplode(Collision col)
+		{
+			if (Collision_IsAboveSpeed(col,minExplosionVelocity))
+			{
+				float dotDir=Collision_HitVelocityAngle(col);
+				//Debug.Log("Collision angle: "+dotDir+" - Relative Velocity: "+col.relativeVelocity.magnitude);
+				float shockForce = (1f -   Mathf.Abs(dotDir)) * col.relativeVelocity.magnitude;
+				bool shouldExplode = shockForce > minExplosionVelocity;
+				Debug.Log("Shock force: "+shockForce+" - Should Explode: "+ shouldExplode);
+				if (shouldExplode)
+					Explode();
+				
+			}
+		}
+
+		float Collision_HitUpwardAngle(Collision col)
+		{
+			Vector3 hitNorm = col.contacts[0].normal;
+			return Vector3.Dot(hitNorm, transform.up);
+		}
+
+		float Collision_HitVelocityAngle(Collision col)
+		{
+			return -Vector3.Dot(col.relativeVelocity.normalized, car.rBody.velocity.normalized);
+		}
+
+		bool Collision_IsFromUnderneath(Collision col)
+		{
+			const float angleThr = 0.9f;
+			float dot = Collision_HitUpwardAngle(col);
+			return dot >= angleThr;
+		}
+
+		bool Collision_IsInMovementDirection(Collision col)
+		{
+			const float angleThr = 0.9f;
+			float dot = Collision_HitVelocityAngle(col);
+			return dot >= angleThr;
+		}
+
+		bool Collision_IsAboveSpeed(Collision col, float metersPerSec)
+		{
+			return col.relativeVelocity.sqrMagnitude > metersPerSec.Squared();
+		}
+
+		void OnCollisionEnter(Collision col)
+		{
+			Collision_CheckShouldExplode(col);
 		}
 	}
 
